4db046f300d5e9d2a495d939a6dcdfbb4f1f0313
Willi Rath <wrath@geomar.de>
2017-03-27 19:37:01 +0200

    Override cartopy_userconfig.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cartopy offline
 
-Minimal package providing a `cartopy_userconfig.update_config` which modifies
-`cartopy.config['pre_existing_data_dir']` to point to a shared copy of data
-needed by Cartopy.
+Makes sure, Cartopy looks for previously downloaded data in
+`$CARTOPY_OFFLINE_SHARED`.
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cartopy_offlinedata',
+      version='0.1',
+      description='Add shared data path to cartopy config',
+      url='https://git.geomar.de/python/cartopy_offlinedata',
+      author='Willi Rath',
+      author_email='wrath@geomar.de',
+      license='MIT',
+      packages=['cartopy_userconfig'],
+      zip_safe=False)
