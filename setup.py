e3844519d356b256eb2bf5518a40d33414e275a8
Willi Rath <wrath@geomar.de>
2017-05-16 19:15:29 +0200

    Do not build conda package

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='cartopy_offlinedata',
-      version='0.1',
+      version='0.2',
       description='Add shared data path to cartopy config',
-      url='https://git.geomar.de/python/cartopy_offlinedata',
+      url='https://git.geomar.de/open-source/cartopy_offlinedata',
       author='Willi Rath',
       author_email='wrath@geomar.de',
       license='MIT',
       packages=['cartopy_userconfig'],
+      scripts=['bin/download_cartopy_data.sh'],
       zip_safe=False)
