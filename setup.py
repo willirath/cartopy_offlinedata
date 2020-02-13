06f3ba9b4a5bfa2cc17e4ef3b007dde45cf30ed2
Willi Rath <willirath@users.noreply.github.com>
2020-02-13 21:14:53 +0100

    Prepare v0.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cartopy_offlinedata',
-      version='0.2.0',
+      version='0.2.1',
       description='Add shared data path to cartopy config',
       url='https://github.com/willirath/cartopy_offlinedata',
       author='Willi Rath',
