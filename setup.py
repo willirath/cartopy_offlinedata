9bbad394700a60e5ed7f1ed67e48e884c8485fc1
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-05-06 07:52:12 +0200

    Add antarctic_ice_shelves_polys to download (#7)
    * Bump the version number to 0.2.3
    
    * Add antarctic_ice_shelves_polys to download
    
    This merge switches to using a python script to do the downloading.
    This let us get the version number more easily from cartopy, as
    well as add features that are not downloaded by default.
    
    I have kind of cheated by downloading the python file we want and
    then importing it as a module.  If a better way exists, I'm
    eager to hear it!

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='cartopy_offlinedata',
-      version='0.2.2',
+      version='0.2.3',
       description='Add shared data path to cartopy config',
       url='https://github.com/willirath/cartopy_offlinedata',
       author='Willi Rath',
       author_email='wrath@geomar.de',
       license='MIT',
       packages=['cartopy_userconfig'],
-      scripts=['bin/download_cartopy_data.sh'],
+      scripts=['bin/download_cartopy_data.py'],
       zip_safe=False)
